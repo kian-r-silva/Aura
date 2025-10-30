9763a539fe1bb8de8f8111b0b26a6437ffea30e4
joaopedro-halllima <158372513+joaopedro-halllima@users.noreply.github.com>
2025-10-29 20:32:20 -0400

    enhanced to pass %

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -1,15 +1,99 @@
 require 'rails_helper'
 
 RSpec.describe "Reviews", type: :request do
-  describe "POST /create" do
-    it "creates a review for an album and redirects" do
-      album = FactoryBot.create(:album)
-      user = FactoryBot.create(:user)
-      params = { review: { rating: 4, body: 'Nice' }, album_id: album.id }
-      # If your controller requires a logged-in user, you may need to set session[:user_id] = user.id
-      post album_reviews_path(album), params: params
-      expect(response).to have_http_status(:redirect)
-    end
+  let(:user) { create(:user) }
+  let(:album) { create(:album) }
+
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
 
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a review and redirects to album" do
+        params = { 
+          review: { rating: 4, comment: 'Great album with amazing songs!' }, 
+          album_id: album.id 
+        }
+        
+        expect {
+          post album_reviews_path(album), params: params
+        }.to change(Review, :count).by(1)
+        
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(album_path(album))
+        follow_redirect!
+        expect(response.body).to include('Review added')
+      end
+
+      it "creates a review with album_title and artists" do
+        params = { 
+          review: { rating: 5, comment: 'Fantastic album!' },
+          album_title: 'Dark Side of the Moon',
+          artists: 'Pink Floyd'
+        }
+        
+        expect {
+          post reviews_path, params: params
+        }.to change(Review, :count).by(1)
+        
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create review with invalid rating" do
+        params = { 
+          review: { rating: 10, comment: 'Too high rating!' }, 
+          album_id: album.id 
+        }
+        
+        expect {
+          post album_reviews_path(album), params: params
+        }.not_to change(Review, :count)
+        
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to include('Unable to save review')
+      end
+
+      it "does not create review with short comment" do
+        params = { 
+          review: { rating: 4, comment: 'Short' }, 
+          album_id: album.id 
+        }
+        
+        expect {
+          post album_reviews_path(album), params: params
+        }.not_to change(Review, :count)
+        
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "does not create review without comment" do
+        params = { 
+          review: { rating: 4, comment: '' }, 
+          album_id: album.id 
+        }
+        
+        expect {
+          post album_reviews_path(album), params: params
+        }.not_to change(Review, :count)
+        
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "does not create review without rating" do
+        params = { 
+          review: { rating: nil, comment: 'This is a comment' }, 
+          album_id: album.id 
+        }
+        
+        expect {
+          post album_reviews_path(album), params: params
+        }.not_to change(Review, :count)
+        
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
