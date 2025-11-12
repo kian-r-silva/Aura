b8339d19ca795b09e0a6a4f12206972cd6b0804d
kian-r-silva <kian.r.silva@columbia.edu>
2025-11-12 13:23:22 -0500

    add recommended songs to main page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,5 +6,10 @@ class SongsController < ApplicationController
   end
 
   def index
+    if current_user
+      @recommendations = AnalyticsService.new(current_user).recommendations_for_user(limit: 10)
+    else
+      @recommendations = []
+    end
   end
 end
