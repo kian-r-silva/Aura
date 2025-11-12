c0f656e3f6c4dbea42f5eca51674aec1cab00650
kian-r-silva <kian.r.silva@columbia.edu>
2025-11-12 14:03:21 -0500

    update db and analytics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_11_193956) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_12_183250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,6 +41,17 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_11_193956) do
     t.bigint "user_id", null: false
     t.index ["song_id"], name: "index_reviews_on_song_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "solid_cache_entries", id: false, force: :cascade do |t|
+    t.integer "byte_size", null: false
+    t.datetime "created_at", null: false
+    t.binary "key", null: false
+    t.bigint "key_hash", null: false
+    t.binary "value", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "songs", force: :cascade do |t|
